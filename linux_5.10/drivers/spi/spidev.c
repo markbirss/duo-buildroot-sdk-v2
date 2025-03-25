f6240e926314ba04a4cec49bf1bee22631605129
Mark Trevor Birss <markbirss@gmail.com>
2025-03-25 09:15:13 +0200

    Update spidev.c

--- linux_5.10/drivers/spi/spidev.c
+++ linux_5.10/drivers/spi/spidev.c
@@ -82,7 +82,7 @@ struct spidev_data {
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
 
-static unsigned bufsiz = 131072; //width * height / 2 * 3 = 115200
+static unsigned bufsiz = 230400;
 module_param(bufsiz, uint, S_IRUGO);
 MODULE_PARM_DESC(bufsiz, "data bytes in biggest supported SPI message");
 
